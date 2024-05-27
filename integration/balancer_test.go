62e6476cff1a41ff55f6ba64f3ca76cb24aa9082
Yevhenii Kovalov <111194749+kovaliovev@users.noreply.github.com>
2024-05-27 10:20:26 +0300

    Add integration tests

--- integration/balancer_test.go
+++ integration/balancer_test.go
@@ -1,32 +1,95 @@
 package integration
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"os"
 	"testing"
 	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
 )
 
 const baseAddress = "http://balancer:8090"
 
-var client = http.Client{
-	Timeout: 3 * time.Second,
+type IntegrationTestSuite struct {
+	suite.Suite
+	client http.Client
 }
 
-func TestBalancer(t *testing.T) {
+type ResponseBody struct {
+	Key   string `json:"key"`
+	Value string `json:"value"`
+}
+
+func TestIntegrationTestSuite(t *testing.T) {
+	suite.Run(t, new(IntegrationTestSuite))
+}
+
+func (s *IntegrationTestSuite) SetupSuite() {
+	s.client = http.Client{
+		Timeout: 3 * time.Second,
+	}
+}
+
+func (s *IntegrationTestSuite) TestBalancer() {
 	if _, exists := os.LookupEnv("INTEGRATION_TEST"); !exists {
-		t.Skip("Integration test is not enabled")
+		s.T().Skip("Integration test is not enabled")
 	}
 
-	// TODO: Реалізуйте інтеграційний тест для балансувальникка.
-	resp, err := client.Get(fmt.Sprintf("%s/api/v1/some-data", baseAddress))
-	if err != nil {
-		t.Error(err)
+	serverTests := []struct {
+		url         string
+		expectedLB  string
+		description string
+	}{
+		{fmt.Sprintf("%s/api/v2/wtf/what-is-it", baseAddress), "server1:8080", "test server #1"},
+		{fmt.Sprintf("%s/api/v1/some-data", baseAddress), "server2:8080", "test server #2"},
+		{fmt.Sprintf("%s/really/good/endpoint", baseAddress), "server3:8080", "test server #3"},
 	}
-	t.Logf("response from [%s]", resp.Header.Get("lb-from"))
+
+	for _, test := range serverTests {
+		s.runServerTest(test.url, test.expectedLB, test.description)
+	}
+
+	// Test repeated request to server #3
+	s.runServerTest(fmt.Sprintf("%s/really/good/endpoint", baseAddress), serverTests[2].expectedLB, "test repeated request to server #3")
+
+	// Test request to check database
+	s.testDatabaseRequest("beshenayaP4olka")
 }
 
-func BenchmarkBalancer(b *testing.B) {
-	// TODO: Реалізуйте інтеграційний бенчмарк для балансувальникка.
+func (s *IntegrationTestSuite) runServerTest(url, expectedLB, description string) {
+	resp, err := s.client.Get(url)
+	assert.NoError(s.T(), err, description)
+	defer resp.Body.Close()
+
+	lbHeader := resp.Header.Get("lb-from")
+	assert.Equal(s.T(), expectedLB, lbHeader, description)
+}
+
+func (s *IntegrationTestSuite) testDatabaseRequest(expectedKey string) {
+	dbResp, err := s.client.Get(fmt.Sprintf("%s/api/v1/some-data?key=%s", baseAddress, expectedKey))
+	assert.NoError(s.T(), err, "test request to check database")
+	defer dbResp.Body.Close()
+
+	var body ResponseBody
+	err = json.NewDecoder(dbResp.Body).Decode(&body)
+	assert.NoError(s.T(), err, "decode response body")
+
+	assert.Equal(s.T(), expectedKey, body.Key, "check response key")
+	assert.NotEmpty(s.T(), body.Value, "check response value")
+}
+
+func (s *IntegrationTestSuite) BenchmarkBalancer(b *testing.B) {
+	if _, exists := os.LookupEnv("INTEGRATION_TEST"); !exists {
+		b.Skip("Integration test is not enabled")
+	}
+
+	for i := 0; i < b.N; i++ {
+		resp, err := s.client.Get(fmt.Sprintf("%s/api/v1/some-data", baseAddress))
+		assert.NoError(b, err, "benchmark request")
+		resp.Body.Close()
+	}
 }
