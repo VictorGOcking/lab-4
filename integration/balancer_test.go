a87942d4359d3f4d92cfb76699f58c1ed90dc294
Mariia Kryvokhata <m.krivokhata@gmail.com>
2024-05-27 16:22:16 +0300

    Fix integration tests

--- integration/balancer_test.go
+++ integration/balancer_test.go
@@ -1,7 +1,6 @@
 package integration
 
 import (
-	"encoding/json"
 	"fmt"
 	"net/http"
 	"os"
@@ -9,14 +8,12 @@ import (
 	"time"
 
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/suite"
 )
 
 const baseAddress = "http://balancer:8090"
 
-type IntegrationTestSuite struct {
-	suite.Suite
-	client http.Client
+var client = http.Client{
+	Timeout: 3 * time.Second,
 }
 
 type ResponseBody struct {
@@ -24,19 +21,9 @@ type ResponseBody struct {
 	Value string `json:"value"`
 }
 
-func TestIntegrationTestSuite(t *testing.T) {
-	suite.Run(t, new(IntegrationTestSuite))
-}
-
-func (s *IntegrationTestSuite) SetupSuite() {
-	s.client = http.Client{
-		Timeout: 3 * time.Second,
-	}
-}
-
-func (s *IntegrationTestSuite) TestBalancer() {
+func TestBalancer(t *testing.T) {
 	if _, exists := os.LookupEnv("INTEGRATION_TEST"); !exists {
-		s.T().Skip("Integration test is not enabled")
+		t.Skip("Integration test is not enabled")
 	}
 
 	serverTests := []struct {
@@ -50,45 +37,29 @@ func (s *IntegrationTestSuite) TestBalancer() {
 	}
 
 	for _, test := range serverTests {
-		s.runServerTest(test.url, test.expectedLB, test.description)
+		runServerTest(t, test.url, test.expectedLB, test.description)
 	}
 
 	// Test repeated request to server #3
-	s.runServerTest(fmt.Sprintf("%s/really/good/endpoint", baseAddress), serverTests[2].expectedLB, "test repeated request to server #3")
-
-	// Test request to check database
-	s.testDatabaseRequest("beshenayaP4olka")
+	runServerTest(t, fmt.Sprintf("%s/really/good/endpoint", baseAddress), serverTests[2].expectedLB, "test repeated request to server #3")
 }
 
-func (s *IntegrationTestSuite) runServerTest(url, expectedLB, description string) {
-	resp, err := s.client.Get(url)
-	assert.NoError(s.T(), err, description)
+func runServerTest(t *testing.T, url, expectedLB, description string) {
+	resp, err := client.Get(url)
+	assert.NoError(t, err, description)
 	defer resp.Body.Close()
 
 	lbHeader := resp.Header.Get("lb-from")
-	assert.Equal(s.T(), expectedLB, lbHeader, description)
+	assert.Equal(t, expectedLB, lbHeader, description)
 }
 
-func (s *IntegrationTestSuite) testDatabaseRequest(expectedKey string) {
-	dbResp, err := s.client.Get(fmt.Sprintf("%s/api/v1/some-data?key=%s", baseAddress, expectedKey))
-	assert.NoError(s.T(), err, "test request to check database")
-	defer dbResp.Body.Close()
-
-	var body ResponseBody
-	err = json.NewDecoder(dbResp.Body).Decode(&body)
-	assert.NoError(s.T(), err, "decode response body")
-
-	assert.Equal(s.T(), expectedKey, body.Key, "check response key")
-	assert.NotEmpty(s.T(), body.Value, "check response value")
-}
-
-func (s *IntegrationTestSuite) BenchmarkBalancer(b *testing.B) {
+func BenchmarkBalancer(b *testing.B) {
 	if _, exists := os.LookupEnv("INTEGRATION_TEST"); !exists {
 		b.Skip("Integration test is not enabled")
 	}
 
 	for i := 0; i < b.N; i++ {
-		resp, err := s.client.Get(fmt.Sprintf("%s/api/v1/some-data", baseAddress))
+		resp, err := client.Get(fmt.Sprintf("%s/api/v1/some-data", baseAddress))
 		assert.NoError(b, err, "benchmark request")
 		resp.Body.Close()
 	}
