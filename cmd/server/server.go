a8b897cd26603db70ba4536a2da1d100df146c31
Mariia Kryvokhata <m.krivokhata@gmail.com>
2024-06-03 21:17:18 +0300

    Added usage and tests of db, changed docker-compose
    - added test for db
    - changed logic of cmd/server
    - changed docker-compose

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"flag"
+	"fmt"
+	"io"
 	"net/http"
 	"os"
 	"strconv"
@@ -14,44 +17,29 @@ import (
 
 var port = flag.Int("port", 8080, "server port")
 
-const confResponseDelaySec = "CONF_RESPONSE_DELAY_SEC"
-const confHealthFailure = "CONF_HEALTH_FAILURE"
+const (
+	databaseURL          = "http://db:8085/db"
+	confResponseDelaySec = "CONF_RESPONSE_DELAY_SEC"
+	confHealthFailure    = "CONF_HEALTH_FAILURE"
+)
 
-func addComplexHandlers(h *http.ServeMux, report Report, paths []string) {
-	for _, path := range paths {
-		h.HandleFunc(path, func(rw http.ResponseWriter, r *http.Request) {
-			respDelayString := os.Getenv(confResponseDelaySec)
-			if delaySec, parseErr := strconv.Atoi(respDelayString); parseErr == nil && delaySec > 0 && delaySec < 300 {
-				time.Sleep(time.Duration(delaySec) * time.Second)
-			}
+type RequestStruct struct {
+	Value string `json:"value"`
+}
 
-			report.Process(r)
-
-			rw.Header().Set("content-type", "application/json")
-			rw.WriteHeader(http.StatusOK)
-			_ = json.NewEncoder(rw).Encode([]string{
-				"1", "2",
-			})
-		})
-	}
+type ResponseStruct struct {
+	Key   string `json:"key"`
+	Value string `json:"value"`
 }
 
 func main() {
-	h := new(http.ServeMux)
+	h := http.NewServeMux()
+	client := http.DefaultClient
 
-	h.HandleFunc("/health", func(rw http.ResponseWriter, r *http.Request) {
-		rw.Header().Set("content-type", "text/plain")
-		if failConfig := os.Getenv(confHealthFailure); failConfig == "true" {
-			rw.WriteHeader(http.StatusInternalServerError)
-			_, _ = rw.Write([]byte("FAILURE"))
-		} else {
-			rw.WriteHeader(http.StatusOK)
-			_, _ = rw.Write([]byte("OK"))
-		}
-	})
-
+	h.HandleFunc("/health", healthHandler)
 	report := make(Report)
 
+	h.HandleFunc("/api/v1/some-data", someDataHandler(client, report))
 	addComplexHandlers(h, report, []string{
 		"/api/v1/wow-data",
 		"/api/v2/wtf/mad-data",
@@ -62,5 +50,99 @@ func main() {
 
 	server := httptools.CreateServer(*port, h)
 	server.Start()
+
+	storeCurrentDate(client)
 	signal.WaitForTerminationSignal()
+}
+
+func healthHandler(rw http.ResponseWriter, r *http.Request) {
+	rw.Header().Set("content-type", "text/plain")
+	if os.Getenv(confHealthFailure) == "true" {
+		rw.WriteHeader(http.StatusInternalServerError)
+		_, _ = rw.Write([]byte("FAILURE"))
+	} else {
+		rw.WriteHeader(http.StatusOK)
+		_, _ = rw.Write([]byte("OK"))
+	}
+}
+
+func someDataHandler(client *http.Client, report Report) http.HandlerFunc {
+	return func(rw http.ResponseWriter, r *http.Request) {
+		key := r.URL.Query().Get("key")
+		if key == "" {
+			http.Error(rw, "Bad Request: key is missing", http.StatusBadRequest)
+			return
+		}
+
+		resp, err := client.Get(fmt.Sprintf("%s/%s", databaseURL, key))
+		if err != nil {
+			http.Error(rw, "Internal Server Error: failed to get data", http.StatusInternalServerError)
+			return
+		}
+		defer func(Body io.ReadCloser) {
+			err := Body.Close()
+			if err != nil {
+
+			}
+		}(resp.Body)
+
+		if resp.StatusCode == http.StatusNotFound {
+			http.Error(rw, "Not Found", http.StatusNotFound)
+			return
+		} else if resp.StatusCode != http.StatusOK {
+			http.Error(rw, "Internal Server Error", http.StatusInternalServerError)
+			return
+		}
+
+		if delaySec, err := strconv.Atoi(os.Getenv(confResponseDelaySec)); err == nil && delaySec > 0 && delaySec < 300 {
+			time.Sleep(time.Duration(delaySec) * time.Second)
+		}
+
+		report.Process(r)
+
+		var body ResponseStruct
+		if err := json.NewDecoder(resp.Body).Decode(&body); err != nil {
+			http.Error(rw, "Internal Server Error: failed to decode response", http.StatusInternalServerError)
+			return
+		}
+
+		rw.Header().Set("content-type", "application/json")
+		rw.WriteHeader(http.StatusOK)
+		_ = json.NewEncoder(rw).Encode(body)
+	}
+}
+
+func storeCurrentDate(client *http.Client) {
+	buff := new(bytes.Buffer)
+	body := RequestStruct{Value: time.Now().Format(time.RFC3339)}
+	if err := json.NewEncoder(buff).Encode(body); err != nil {
+		return
+	}
+
+	resp, err := client.Post(fmt.Sprintf("%s/victorgocking", databaseURL), "application/json", buff)
+	if err != nil {
+		return
+	}
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+
+		}
+	}(resp.Body)
+}
+
+func addComplexHandlers(h *http.ServeMux, report Report, paths []string) {
+	for _, path := range paths {
+		h.HandleFunc(path, func(rw http.ResponseWriter, r *http.Request) {
+			if delaySec, err := strconv.Atoi(os.Getenv(confResponseDelaySec)); err == nil && delaySec > 0 && delaySec < 300 {
+				time.Sleep(time.Duration(delaySec) * time.Second)
+			}
+
+			report.Process(r)
+
+			rw.Header().Set("content-type", "application/json")
+			rw.WriteHeader(http.StatusOK)
+			_ = json.NewEncoder(rw).Encode([]string{"1", "2"})
+		})
+	}
 }
